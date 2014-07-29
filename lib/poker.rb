d129084186213490713f198868c84621d270c3e0
Matthew <mattjunk85@yahoo.com>
2014-07-29 16:36:06 -0700

    recognizes every kind of hand without error

--- lib/poker.rb
+++ lib/poker.rb
@@ -3,27 +3,38 @@ def poker_hand(hand)
   suites = []
   result = ""
   hand.each do |card|
+    ## Converts face-cards to numbers and seperates card value and suite into seperate arrays ##
     numbers << card.gsub(/["JKQA"]/,'J'=>'11','Q'=>'12','K'=>'13','A'=>'14').to_i
     suites << card.slice(-1)
   end
+  ## Sorts the card values in descending order ##
   numbers.sort!.reverse!
 
   if numbers[0]-4 == numbers[4] && numbers.uniq.length == 5
     result = "straight"
   end
-  if suites.all? {|x| x == suites[0]}
-    puts "true"
-    result = result + " flush"
-  elsif numbers.count(numbers[1]) + numbers.count(numbers[3]) == 5
+  if numbers.count(numbers[1]) + numbers.count(numbers[3]) == 5
     result = "full-house"
   elsif numbers.uniq.length == 4
     result = "two of a kind"
   elsif numbers.uniq.length == 3
-    result = "three of a kind"
+    if numbers.count(numbers[0]) == 2 || numbers.count(numbers[1]) == 2
+      result = "two pair"
+    else
+      result = "three of a kind"
+    end
   elsif numbers.uniq.length == 2
     result = "four of a kind"
   end
-  result.strip
+
+  if suites.all? {|x| x == suites[0]}
+    result = result + " flush"
+  end
+
+  if result == "straight flush" && (numbers.reduce :+) == 60
+    "royal flush"
+  else
+    result.strip
+  end
 end
 
-poker_hand(['9C', '3D', '3H', '3C', '3C'])
--- spec/poker_spec.rb
+++ spec/poker_spec.rb
@@ -15,7 +15,7 @@ describe "poker_hand" do
     poker_hand(['9C', 'JD', 'KH', 'QC', '10C']).should eql "straight"
   end
   it "recognizes a straight flush" do
-    poker_hand(['10D', 'JD', 'KD', 'QD', 'AD']).should eql "straight flush"
+    poker_hand(['5D', '6D', '7D', '8D', '9D']).should eql "straight flush"
   end
   it "recognizes 2 of a kind" do
     poker_hand(['9C', '9D', 'KH', 'QC', '10C']).should eql "two of a kind"
@@ -28,5 +28,15 @@ describe "poker_hand" do
   end
   it "recognizes a full-house" do
     poker_hand(['9C', '9D', '9H', 'KC', 'KC']).should eql "full-house"
+  end
+  it "recognizes a royal flush" do
+    poker_hand(['10S', 'JS', 'QS', 'KS', 'AS']).should eql "royal flush"
+  end
+  it "recognizes a two pair" do
+    poker_hand(['9S', '9S', '3C', '4S', '4S']).should eql "two pair"
+  end
+  it "recognizes a flush combinations" do
+    poker_hand(['9S', '9S', '3S', '4S', '4S']).should eql "two pair flush"
+    poker_hand(['9S', '9S', '9S', '9S', '2S']).should eql "four of a kind flush"
   end
 end
