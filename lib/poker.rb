a7ebbc11c74a5bbea66ec1f470f04ec05cbb9b59
Matthew <mattjunk85@yahoo.com>
2014-07-29 15:12:56 -0700

    recognizes straights without face-cards

--- lib/poker.rb
+++ lib/poker.rb
@@ -2,12 +2,19 @@ def poker_hand(hand)
   numbers = []
   suites = []
   hand.each do |card|
-    numbers << card.to_i
+    numbers << card.gsub(/["JKQA"]/,'J'=>'10','Q'=>'11','K'=>'12','A'=>'13').to_i
     suites << card.slice(-1)
   end
-  if suites.all? {|x| x == suites[0]}
-    "flush"
+  numbers.sort!.reverse!
+
+
+  if numbers[0]-4 == numbers[4] && numbers.uniq.length == 5
+    result = "straight"
   end
+  if suites.all? {|x| x == suites[0]}
+    result = "flush"
+  end
+  result
 end
 
 poker_hand(['2D', '3C', '4C', '5C', '6C'])
--- spec/poker_spec.rb
+++ spec/poker_spec.rb
@@ -3,6 +3,9 @@ require ('poker')
 
 describe "poker_hand" do
   it "recognizes a flush without face-cards" do
-    poker_hand(['2C', '3C', '4C', '5C', '6C']).should eql "flush"
+    poker_hand(['2C', '7C', '4C', '5C', '6C']).should eql "flush"
+  end
+  it "recognizes a straight without face-cards" do
+    poker_hand(['2C', '3D', '4H', '5C', '6C']).should eql "straight"
   end
 end
